22cc18389674e2584643581c65e2f7c075a9b38e
Micael Fernandes <micaelnandes21@hotmail.com>
2022-04-08 00:03:01 -0300

    control texts

--- static/game.js
+++ static/game.js
@@ -1,13 +1,13 @@
 console.log(" Olá. Esta é a bola pulante !");
 
-const canvas = document.getElementById("GameCanvas");
-const contexto = canvas.getContext('2d');
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
-const mouse = new Vector(30, 30);
-var mouseClicked = false;
-var toggleB = document.getElementById("labelToggle")
-var mouseOn = true;
+const canvas = document.getElementById("GameCanvas");   // seleciona o objeto canvas da pagina HTML
+const contexto = canvas.getContext('2d');   // cria o contexto para desenho no canvas
+var width = canvas.width = window.innerWidth;   // define o tamanho do canvas
+var height = canvas.height = window.innerHeight;    // defina a altura do canvas
+const mouse = new Vector(30, 30);   // inicia a posição do mouse
+var mouseClicked = false;   // variavel para controle do clique do mouse
+var toggleB = document.getElementById("labelToggle")    // inicia botão do ambiente
+var mouseOn = true; // variavel para controle do mouse sobre o botão
 
 toggleB.addEventListener('mouseover', () => {
     mouseOn = false;
@@ -64,23 +64,23 @@ function ballBorderCollision(ball) {
         ball.velocity.invertY()
         ball.aceleration.invertY()
         if (ball.gravity == null) {
-            updateBackground('', true)    
+            updateBackground('', true)
         }
-        
+
     }
 }
 
 const ball = new Circle('ball', 10, new Vector(width / 2, height / 2))
 ball.setFriction(.98)
 ball.setVelocity(new Vector(0, 0))
-canvas.addEventListener('wheel',(e) => {
-    if(ball.radius + e.deltaY/20 >= 0){
-        ball.radius += e.deltaY/20;
+canvas.addEventListener('wheel', (e) => {
+    if (ball.radius + e.deltaY / 20 >= 0) {
+        ball.radius += e.deltaY / 20;
     }
-    if(ball.radius >= 120){
+    if (ball.radius >= 120) {
         ball.radius = 120
     }
-    if(ball.radius <= 5){
+    if (ball.radius <= 5) {
         ball.radius = 5
     }
 })
@@ -88,20 +88,29 @@ canvas.addEventListener('wheel',(e) => {
 function Buttontoggle() {
     if (document.getElementById('toggle').checked) {
         ball.setGravity(new Vector(0, 1))
-    }else {
+    } else {
         ball.setGravity(null)
     }
 }
 
+var c = 0
 function loop() {
     updateDisplay();
     contexto.clearRect(0, 0, width, height);
+    if(c < 1){
+        contexto.font = '12px Arial'
+        contexto.textAlign = 'center'
+        contexto.fillText("Use o scroll do mouse para alterar o tamanho da bola", 150, 20)
+        contexto.fillText("Clique para movimentar a bola", 89, 40)
+        delete c
+    }
     ball.draw(contexto);
     ballBorderCollision(ball);
 
     const ballLine = { x0: ball.position.x, y0: ball.position.y, x1: mouse.x, y1: mouse.y };
     let impulseVector = new Vector(ballLine.x1, ballLine.y1).subtract(new Vector(ballLine.x0, ballLine.y0))
     if (mouseClicked) {
+        c+=1
         ball.setVelocity(impulseVector)
         mouseClicked = false;
     }
